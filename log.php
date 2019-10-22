2b57ef5f5b33ffc690aee3f83d69f73919a59605
Juan Argueta <msjuanchila@gmail.com>
2019-10-22 12:28:05 -0500

    agrege el log

---
+++ log.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+echo '
+
+<form name="formulario" method="post" action="login.php">
+
+id: <input type="text" name="id" value="">
+User: <input type="text" name="user" value="">
+Pass: <input type="text" name="password" value="">
+
+<input type="submit" />
+
+</form>
+
+';
+
+
+?>
